ed8df0cae704a3b4e1fb0d55ccbbcda8cbe5f1c6
Scott Hughes <scott.hughes1ibm.com@Scotts-MacBook-Pro.local>
2020-06-14 21:53:58 +0100

    Updates

---
+++ precache-manifest.e1e05a8d6bac51537eeda90eb0f63bad.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f4bf9a02aac4551c02b4dba04f461f3",
+    "url": "/scott-macdonald-portfolio/index.html"
+  },
+  {
+    "revision": "5d6c739154388df5ca8d",
+    "url": "/scott-macdonald-portfolio/static/css/main.9f057527.chunk.css"
+  },
+  {
+    "revision": "5e7c050e22a25ac7df01",
+    "url": "/scott-macdonald-portfolio/static/js/2.9051e4a0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/scott-macdonald-portfolio/static/js/2.9051e4a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d6c739154388df5ca8d",
+    "url": "/scott-macdonald-portfolio/static/js/main.401818c8.chunk.js"
+  },
+  {
+    "revision": "6807f3d392a1f35a9f14",
+    "url": "/scott-macdonald-portfolio/static/js/runtime-main.0358efe3.js"
+  },
+  {
+    "revision": "32cacf1f2fd92efc6afae350827009d5",
+    "url": "/scott-macdonald-portfolio/static/media/avatar.32cacf1f.png"
+  },
+  {
+    "revision": "0a6190521035890592cd0adfabb68fac",
+    "url": "/scott-macdonald-portfolio/static/media/banner.0a619052.jpg"
+  },
+  {
+    "revision": "af92ca9d711562d4e741538a78c0ef9c",
+    "url": "/scott-macdonald-portfolio/static/media/event4.af92ca9d.png"
+  },
+  {
+    "revision": "35b415ac93c52b4e8c5c3d601c899825",
+    "url": "/scott-macdonald-portfolio/static/media/event5.35b415ac.png"
+  },
+  {
+    "revision": "248760e512cc7f4abab667d61b1475d9",
+    "url": "/scott-macdonald-portfolio/static/media/events1.248760e5.png"
+  },
+  {
+    "revision": "ada76253220972a2d21a9c0d5a0fbf04",
+    "url": "/scott-macdonald-portfolio/static/media/events2.ada76253.png"
+  },
+  {
+    "revision": "9c22a7c3bf131df05351d9c6efa608e6",
+    "url": "/scott-macdonald-portfolio/static/media/px.9c22a7c3.png"
+  },
+  {
+    "revision": "f8da1fcd4fde82f4e67efd2d50b12ad7",
+    "url": "/scott-macdonald-portfolio/static/media/qma.f8da1fcd.png"
+  }
+]);
