3ffaf4072814233d942557927dae96d336527d34
Scott MacDonald <scottmacdonald@Scotts-MacBook-Pro.local>
2020-12-19 22:43:16 +0000

    add in fade animation

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import FadeIn from "react-fade-in";
 
 import ImageBanner from "components/elements/ImageBanner";
 import imageForBanner from "assets/banner.png";
@@ -7,17 +8,16 @@ import devtext from "assets/developertext.jpg";
 const Home = () => {
   return (
     <div className="portfolio-homepage">
-      <ImageBanner
-        bannerImage={imageForBanner}
-        altText="Picture of me and a collegue pair programming."
-      />
-      <div className="portfolio-homepage__text">
-
-        <img src={devtext} alt="Liverpool Based Developer"/>
-
-      </div>
+      <FadeIn delay={400} transitionDuration={800}>
+        <ImageBanner
+          bannerImage={imageForBanner}
+          altText="Picture of me and a collegue pair programming."
+        />
+        <div className="portfolio-homepage__text">
+          <img src={devtext} alt="Liverpool Based Developer" />
+        </div>
+      </FadeIn>
     </div>
-    
   );
 };
 
