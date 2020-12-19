23fed792a8f1bea20b2f4d25d7ae4f8a5262e866
Scott MacDonald <scottmacdonald@Scotts-MacBook-Pro.local>
2020-12-19 22:31:14 +0000

    new post examples added

--- src/components/elements/Post.js
+++ src/components/elements/Post.js
@@ -1,12 +1,23 @@
 import React from "react";
-import { Grid, Container } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 
-const Post = () => {
+const Post = (copy1, copy2, imgsrc, alttext) => {
   return (
     <div>
-      <div></div>
+      <Grid container spacing={6}>
+        <Grid item xs={6} sm={6}>
+          {copy1 && <div>{copy1}</div>}
+          <br />
+          {copy2 && <div>{copy2}</div>}
+        </Grid>
+        {imgsrc &&
+        <Grid item xs={6} sm={6}>
+          <img src={imgsrc} width="360px" height="640px" alt={alttext} />
+        </Grid>
+}
+      </Grid>
     </div>
   );
 };
 
-export default Blog; 
+export default Post;
--- src/components/pages/blogposts/post1.js
+++ src/components/pages/blogposts/post1.js
@@ -1,45 +1,9 @@
 import React from "react";
-import { Grid, Container } from "@material-ui/core";
 
-import tester from "assets/tester.png";
+import Post from "components/elements/Post";
 
 const Post1 = () => {
-  return (
-    <Container maxWidth="xl">
-      <Grid container spacing={6}>
-        <Grid item xs={6} sm={6}>
-          <div>
-
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque euismod efficitur est quis sollicitudin. Pellentesque rhoncus orci sed dictum elementum. Mauris a elementum risus. Cras sit amet metus sit amet nunc malesuada ornare eget sit amet felis. Etiam a lacinia nunc, ac dictum elit. Proin blandit ultrices mi, eget varius sem tincidunt vel. Aliquam erat volutpat. Phasellus sit amet nulla mi. Nulla facilisi.
-
-Fusce molestie laoreet feugiat. Donec sem neque, consectetur at nulla et, luctus efficitur ligula. Fusce lobortis lacus eget erat venenatis cursus. Etiam eget auctor ex. Pellentesque eget feugiat lectus, in iaculis nisl. Morbi bibendum nunc eget arcu pulvinar imperdiet eu consequat ante. Phasellus at arcu turpis. Cras augue sapien, venenatis eget egestas vel, pellentesque vel lectus. Sed eget porttitor ante. Sed et nunc vitae leo sollicitudin rhoncus. Pellentesque feugiat sed nisi ac ultricies. Quisque et eros ornare, vehicula ipsum vitae, lobortis elit. Nulla lobortis mauris eget dictum viverra. Aenean quis egestas augue, id vestibulum felis. Suspendisse maximus metus eget pharetra suscipit. Nunc at facilisis lectus, eu dignissim urna.
-
-Pellentesque laoreet euismod mauris non volutpat. Etiam quis porttitor nulla. Sed dui lectus, vulputate eget feugiat vitae, fringilla vel tellus. Integer sed justo venenatis, condimentum sem non, tempor purus. Vivamus suscipit, turpis sed aliquet sollicitudin, elit libero bibendum orci, vitae pulvinar urna mauris ac dolor. Maecenas et risus mauris. Curabitur ullamcorper semper diam ut sollicitudin. Nulla velit ante, accumsan in eleifend quis, efficitur eu enim. Morbi ac hendrerit neque, quis auctor est. Proin nec dolor eget felis accumsan ullamcorper sed a arcu. Quisque dignissim gravida libero sed ornare.
-
-Nam sed consectetur dui. Quisque molestie nunc sed tortor aliquet, sed tempus lacus maximus. Cras blandit quis odio non sagittis. Donec cursus at velit et ultricies. Integer eu porttitor tellus. Vestibulum porttitor nunc ac enim commodo sollicitudin. Integer ex tortor, faucibus a venenatis vel, rutrum quis nibh. Suspendisse ac tincidunt diam. Praesent fringilla nulla et est posuere, quis elementum neque rutrum. Maecenas bibendum, nisl quis tempus accumsan, orci purus consequat elit, vitae aliquet orci dui sollicitudin nisl. Maecenas non velit turpis. Nulla placerat libero vel est finibus, quis mollis eros venenatis. Morbi vel ipsum aliquam, scelerisque sapien nec, congue lorem. Cras pharetra nisl vitae diam aliquam tempus. Proin nec turpis blandit, eleifend neque at, dignissim odio. Praesent id est ipsum.
-
-          </div>
-          <br/>
-          <div>
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque euismod efficitur est quis sollicitudin. Pellentesque rhoncus orci sed dictum elementum. Mauris a elementum risus. Cras sit amet metus sit amet nunc malesuada ornare eget sit amet felis. Etiam a lacinia nunc, ac dictum elit. Proin blandit ultrices mi, eget varius sem tincidunt vel. Aliquam erat volutpat. Phasellus sit amet nulla mi. Nulla facilisi.
-
-Fusce molestie laoreet feugiat. Donec sem neque, consectetur at nulla et, luctus efficitur ligula. Fusce lobortis lacus eget erat venenatis cursus. Etiam eget auctor ex. Pellentesque eget feugiat lectus, in iaculis nisl. Morbi bibendum nunc eget arcu pulvinar imperdiet eu consequat ante. Phasellus at arcu turpis. Cras augue sapien, venenatis eget egestas vel, pellentesque vel lectus. Sed eget porttitor ante. Sed et nunc vitae leo sollicitudin rhoncus. Pellentesque feugiat sed nisi ac ultricies. Quisque et eros ornare, vehicula ipsum vitae, lobortis elit. Nulla lobortis mauris eget dictum viverra. Aenean quis egestas augue, id vestibulum felis. Suspendisse maximus metus eget pharetra suscipit. Nunc at facilisis lectus, eu dignissim urna.
-
-Pellentesque laoreet euismod mauris non volutpat. Etiam quis porttitor nulla. Sed dui lectus, vulputate eget feugiat vitae, fringilla vel tellus. Integer sed justo venenatis, condimentum sem non, tempor purus. Vivamus suscipit, turpis sed aliquet sollicitudin, elit libero bibendum orci, vitae pulvinar urna mauris ac dolor. Maecenas et risus mauris. Curabitur ullamcorper semper diam ut sollicitudin. Nulla velit ante, accumsan in eleifend quis, efficitur eu enim. Morbi ac hendrerit neque, quis auctor est. Proin nec dolor eget felis accumsan ullamcorper sed a arcu. Quisque dignissim gravida libero sed ornare.
-
-Nam sed consectetur dui. Quisque molestie nunc sed tortor aliquet, sed tempus lacus maximus. Cras blandit quis odio non sagittis. Donec cursus at velit et ultricies. Integer eu porttitor tellus. Vestibulum porttitor nunc ac enim commodo sollicitudin. Integer ex tortor, faucibus a venenatis vel, rutrum quis nibh. Suspendisse ac tincidunt diam. Praesent fringilla nulla et est posuere, quis elementum neque rutrum. Maecenas bibendum, nisl quis tempus accumsan, orci purus consequat elit, vitae aliquet orci dui sollicitudin nisl. Maecenas non velit turpis. Nulla placerat libero vel est finibus, quis mollis eros venenatis. Morbi vel ipsum aliquam, scelerisque sapien nec, congue lorem. Cras pharetra nisl vitae diam aliquam tempus. Proin nec turpis blandit, eleifend neque at, dignissim odio. Praesent id est ipsum.
-
-</div>
-        </Grid>
-        <Grid item xs={6} sm={6}>
-
-        <img src={tester} width="360px" height="640px"  />
-
-        </Grid>
-      </Grid>
-    </Container>
-  );
+  return <Post />;
 };
 
 export default Post1;
